dfba5755c7c0f8dd324323c7052a1aa965b7672e
yusrililhm <yusrililham62@gmail.com>
2023-08-18 14:53:02 +0700

    session 2 update

--- session-2/main.go
+++ session-2/main.go
@@ -1,10 +1,43 @@
 package main
 
+import "fmt"
+
 func main() {
 	// if-else
+
+	name := "momo"
+
+	if name == "momo" {
+		fmt.Println("hai momo")
+	} else {
+		fmt.Println("kamu siapa?")
+	}
+
 	// switch case
+	switch name {
+	case "momo":
+		fmt.Println("hai momo")
+	default:
+		fmt.Println("kamu siapa?")
+	}
+
 	// looping
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d", i)
+	}
+
 	// array
+	array := [3]string{"momo", "sana", "mina"}
+	fmt.Println(array)
+
 	// slice
+	slices := []string{"momo", "sana", "mina"}
+	fmt.Println(slices)
+
 	// map
+	student := map[string]string{
+		"name": "momo",
+		"hobby": "dance",
+	}
+	fmt.Println(student)
 }
